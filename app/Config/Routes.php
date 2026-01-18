b8338100a7da00bff634348ee58fd048862db3cb
NOezbek <n.oezbek@viscom-online.de>
2026-01-18 14:46:42 +0100

    sinnloser dreck weg

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -15,10 +15,8 @@ $routes->get('/bootsverleih', 'Home::bootsverleih');   // ← Bootsverleih
 $routes->get('/kundenverwaltung', 'Home::kundenverwaltung');   // ← Kundenverwaltung
 
 //Data
-$routes->group('', ['filter' => 'api'], function($routes) {
-    $routes->get('/kundenverwaltung/load', 'KundenverwaltungController::loadKundenverwaltung');
-    $routes->post('/kundenverwaltung/save', 'KundenverwaltungController::saveKundenverwaltung');
-    $routes->post('/kundenverwaltung/delete', 'KundenverwaltungController::deleteKundenverwaltung');
-});
+$routes->get('/kundenverwaltung/load', 'KundenverwaltungController::loadKundenverwaltung');
+$routes->post('/kundenverwaltung/save', 'KundenverwaltungController::saveKundenverwaltung');
+$routes->post('/kundenverwaltung/delete', 'KundenverwaltungController::deleteKundenverwaltung');
 
 // $routes->setAutoRoute(true);      
